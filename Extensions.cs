a75dfd49e2549c0f72641bab630730e1712b521a
Ulvi Imanov <imanovulvi90@gmail.com>
2024-12-25 09:15:05 +0400

    Add project files.

---
+++ Extensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerService1
+{
+    public static class Extensions
+    {
+        public static async Task<IServiceCollection> AddWorkerService(this IServiceCollection services) 
+        {
+            var interfaceType = typeof(IHostService);
+
+          //  var types = Assembly.GetExecutingAssembly().GetTypes()
+                //.Where(t => interfaceType.IsAssignableFrom(t) && t.IsClass);
+
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(a => a.GetTypes())
+            .Where(t => interfaceType.IsAssignableFrom(t) && t.IsClass);
+            foreach (var type in types)
+            {
+                
+            
+                // Dinamik olaraq instansiya yaradın
+                var instance = Activator.CreateInstance(type) as IHostService;
+                await instance?.ExecuteAsync(); // Metodu işə sal
+            }
+            return services;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+﻿#Worker Service
+##Bu paket .net core proyektleri ucun yazilmisdir
+##Proyektde Program.cs file asagdaki configurasiyani elave etmek
+```CSharp
+builder.Services.AddWorkerService();
+```
